3e59a29a4a6d4503ce03112baff73e79a5f04c49
guochao <1303993457@qq.com>
2019-08-08 19:00:29 +0800

    refactor(*): curl开启线程下载数据

---
+++ ffmpeg_curl/HttpClient.h
@@ -0,0 +1,225 @@
+#pragma once
+
+#include <curl/curl.h>
+#include <string>
+#include <iostream>
+
+#include "Time.h"
+
+typedef size_t (*CurlOptionWriteFunction)(void *, size_t, size_t, void *);
+typedef int (*CurlOptionProgressFunction)(void *, double, double, double, double);
+using FunctionPtr = size_t (*)(void *, size_t, size_t, void *);
+
+namespace duobei {
+
+int OnDebug(CURL *, curl_infotype type, char *data, size_t size, void *);
+class HttpClient {
+public:
+    struct EasyCURL {
+        static int __count;
+        CURL *curl = nullptr;
+
+        int sockopt_data = 1;
+        static int sockopt_callback(void *clientp, curl_socket_t curlfd,
+                                    curlsocktype purpose) {
+        	int value = *(int *)clientp;
+            setsockopt(curlfd, SOL_SOCKET, SO_NOSIGPIPE, &value, sizeof(value));
+            return CURL_SOCKOPT_OK;
+        }
+
+        EasyCURL(bool debug) {
+            if (++__count == 1) {
+                /* Must initialize libcurl before any threads are started */
+                curl_global_init(CURL_GLOBAL_ALL);
+            }
+
+            curl = curl_easy_init();
+            if (debug) {
+                curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
+                curl_easy_setopt(curl, CURLOPT_DEBUGFUNCTION, OnDebug);
+                curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+            }
+
+            //https://curl.haxx.se/libcurl/c/CURLOPT_SOCKOPTDATA.html
+            curl_easy_setopt(curl, CURLOPT_SOCKOPTFUNCTION, sockopt_callback);
+            curl_easy_setopt(curl, CURLOPT_SOCKOPTDATA, &sockopt_data);
+        }
+        explicit EasyCURL()
+            : EasyCURL(false) {}
+        ~EasyCURL() {
+            FreeHeaders();
+            curl_easy_cleanup(curl);
+        }
+
+        std::string Escape(const std::string &source) {
+            char *out = curl_easy_escape(curl, source.c_str(), source.length());
+            std::string dest(out);
+            if (out) {
+                curl_free(out);
+            }
+            return dest;
+        }
+
+        struct curl_slist *headers = nullptr;
+        // "Content-Type: application/json"
+        void setContentType(const std::string &c) {
+            headers = curl_slist_append(headers, "Expect:");
+            headers = curl_slist_append(headers, c.c_str());
+            curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+        }
+
+        void FreeHeaders() {
+            if (headers) {
+                curl_slist_free_all(headers);
+            }
+        }
+
+        CURLcode Perform() {
+            char errbuf[CURL_ERROR_SIZE] = {0};
+            curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, errbuf);
+            return curl_easy_perform(curl);  // todo: crash 偶现
+        }
+    };
+
+    struct DownloadBuffer {
+        const void *data;
+        int size;
+
+        explicit DownloadBuffer(uint8_t *ptr)
+            : data(ptr), size(0) {}
+        size_t eatData(void *buffer, size_t len) {
+            memcpy((char *)data + size, buffer, len);
+            size += len;
+            return len;
+        }
+        static size_t callback(void *pBuffer, size_t nSize, size_t nMemByte, void *pParam) {
+            //把下载到的数据以追加的方式写入文件(一定要有a，否则前面写入的内容就会被覆盖了)
+            auto that = static_cast<DownloadBuffer *>(pParam);
+            if (!that) {
+                return 0;
+            }
+            //WriteDebugLog("callback::[%d][%lu]", buffer->size, nMemByte);
+            return that->eatData(pBuffer, nSize * nMemByte);
+        }
+    };
+
+    static std::string UserAgent;
+    HttpClient() = default;
+    // note：目前移动端同步到pc的curl方式，移动端原来的方式暂时保留
+
+    ~HttpClient() = default;
+
+public:
+    /**
+	* @brief HTTP POST请求
+	* @param url 输入参数,请求的Url地址,如:http://www.baidu.com
+	* @param forms 输入参数,使用如下格式para1=val1&para2=val2&…
+	* @param response 输出参数,返回的内容
+	* @return 返回是否Post成功
+	*/
+    int Post(const std::string &url, const std::string &forms, std::string &response);
+
+    /**
+	* @brief HTTP GET请求
+	* @param url 输入参数,请求的Url地址,如:http://www.baidu.com
+	* @param response 输出参数,返回的内容
+	* @return 返回是否Get成功
+	*/
+    int Get(const std::string &url, std::string &response);
+
+    /**
+	* @brief HTTPS POST请求,无证书版本
+	* @param url 输入参数,请求的Url地址,如:https://www.alipay.com
+	* @param forms 输入参数,使用如下格式para1=val1&para2=val2&…
+	* @param strResponse 输出参数,返回的内容
+	* @param certPath 输入参数,为CA证书的路径.如果输入为NULL,则不验证服务器端证书的有效性.
+	* @return 返回是否Post成功
+	*/
+    int Posts(const std::string &url, const std::string &forms, std::string &strResponse,
+              const std::string &certPath = NULL);
+
+    /**
+	* @brief HTTPS GET请求,无证书版本
+	* @param url 输入参数,请求的Url地址,如:https://www.alipay.com
+	* @param response 输出参数,返回的内容
+	* @param certPath 输入参数,为CA证书的路径.如果输入为NULL,则不验证服务器端证书的有效性.
+	* @return 返回是否Post成功
+	*/
+    int Gets(const std::string &url, std::string &response, const std::string &certPath = NULL);
+    /**
+	 * 获取文件大小
+	 */
+    double getHttpFileSize(const std::string &url);  //CurlOptionWriteFunction
+    /**
+	 * @param url 地址
+	 * @param begin 开始位置
+	 * @param end  结束位置
+	 * @param wfun 下载文件读取函数
+	 * @param progress 进度回调函数
+	 */
+    // todo: PC端回放下载文件
+    int Download(const std::string &url, size_t begin, size_t end, DownloadBuffer &buffer, CurlOptionProgressFunction progress);
+    int Download(const std::string &url, size_t begin, size_t end, DownloadBuffer &buffer);
+
+    int downFile(const char *url, CurlOptionWriteFunction f, void *v);
+
+    static std::string UrlEncode(const std::string &source) {
+        EasyCURL c;
+        return c.Escape(source);
+    }
+
+private:
+    bool debug_ = false;
+};
+
+struct CURLAgent {
+    using EasyCURL = HttpClient::EasyCURL;
+    using DownloadBuffer = HttpClient::DownloadBuffer;
+    EasyCURL c;
+    struct curl_slist *list = nullptr;
+    char buf_[64] = {0};
+    CURLAgent() {
+    }
+    ~CURLAgent() {
+    }
+
+    int Download(const std::string &url, size_t begin, size_t end, DownloadBuffer &buffer) {
+        if (!c.curl) {
+            return CURLE_FAILED_INIT;
+        }
+
+        snprintf(buf_, 64, "Range: bytes=%zu-%zu", begin, end);
+        curl_easy_setopt(c.curl, CURLOPT_URL, url.c_str());
+        list = curl_slist_append(list, buf_);
+        curl_easy_setopt(c.curl, CURLOPT_SSL_VERIFYPEER, 0L);  //设置不验证证书
+        curl_easy_setopt(c.curl, CURLOPT_SSL_VERIFYHOST, 0L);  //不验证host
+        curl_easy_setopt(c.curl, CURLOPT_HTTPHEADER, list);
+        //设置接收数据的回调
+        curl_easy_setopt(c.curl, CURLOPT_WRITEFUNCTION, buffer.callback);  // CurlOptionWriteFunction
+        curl_easy_setopt(c.curl, CURLOPT_WRITEDATA, &buffer);
+        curl_easy_setopt(c.curl, CURLOPT_TIMEOUT_MS, 2000);  //curl最长执行秒数
+
+        //curl_easy_setopt(c.curl, CURLOPT_INFILESIZE, lFileSize);
+        //curl_easy_setopt(c.curl, CURLOPT_HEADER, 1);
+        //curl_easy_setopt(c.curl, CURLOPT_NOBODY, 1);
+        curl_easy_setopt(c.curl, CURLOPT_NOSIGNAL, 1);
+        // 设置重定向的最大次数
+        curl_easy_setopt(c.curl, CURLOPT_MAXREDIRS, 5);
+        // 设置301、302跳转跟随location
+        curl_easy_setopt(c.curl, CURLOPT_FOLLOWLOCATION, 1);
+        //设置进度回调函数
+        //curl_easy_getinfo(c.curl,  CURLINFO_CONTENT_LENGTH_DOWNLOAD, &lFileSize);
+        //curl_easy_setopt(c.curl, CURLOPT_PROGRESSDATA, g_hDlgWnd);
+        //开始执行请求
+        CURLcode retcCode = c.Perform();
+        if (retcCode != CURLE_OK) {
+            std::cout << "err = " << retcCode << ", msg = " << curl_easy_strerror(retcCode) << std::endl;
+        }
+        //清理curl，和前面的初始化匹配
+        curl_slist_free_all(list); /* free the list again */
+        list = nullptr;
+        return retcCode;
+    }
+};
+
+}  // namespace duobei
---
+++ ffmpeg_curl/HttpFile.cpp
@@ -0,0 +1,286 @@
+#include "HttpFile.h"
+
+namespace duobei {
+
+void HttpFile::DownloadThread() {
+    CURLAgent curl_agent;
+    while (worker.running) {
+        //TODO：添加检查代码，检查map中是否有过期数据过期后删除
+        //如果大于文件大小,则退出
+        std::unique_lock<std::mutex> lck(close_Mx_);
+        if (!worker.running) {
+            return;
+        }
+        if (worker.Eof()) {
+	        lck.unlock();
+            std::unique_lock<std::mutex> cl(synchronizer.mtx);
+            synchronizer.cond.wait(cl);
+            continue;
+        }
+        std::unique_lock<std::mutex>  cache_lock(cache_.mtx);
+        auto key = worker.cache_index;
+        auto left = worker.cache_index * kCacheLength;
+        //如果map中小于预约数量则等待
+        if (worker.SeekAtLeft() && (left - worker.current_position) >= kCacheBlockSum * kCacheLength && cache_.full()) {
+	        cache_lock.unlock();
+	        lck.unlock();
+            std::unique_lock<std::mutex> cl(synchronizer.mtx);
+            synchronizer.cond.wait(cl);
+            continue;
+        }
+
+        if (cache_.buffers.find(worker.cache_index) != cache_.buffers.end()) {
+            // 存在
+            ++worker.cache_index;
+	        cache_lock.unlock();
+	        lck.unlock();
+            continue;
+        }
+	    cache_lock.unlock();
+
+        auto right_ = left + kCacheLength;
+        auto right = right_ > worker.file_size ? worker.file_size - 1 : right_ - 1;
+        auto buf_ = Buffer::New(left, right);
+        ++worker.cache_index;
+
+        // todo: bf 的有效期问题
+        HttpClient::DownloadBuffer bf(buf_->data);
+        int ret = curl_agent.Download(url_, buf_->begin, buf_->end, bf);
+        if (ret != 0 || bf.size == 0) {
+            // 失败
+            if (worker.cache_index > 0) {
+                --worker.cache_index;
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	        //lck.unlock();
+            continue;
+        }
+	    cache_lock.lock();
+        cache_.buffers.emplace(key, std::move(buf_));
+	    cache_lock.unlock();
+	    //lck.unlock();
+    }
+    // WriteDebugLog("DownloadThread quit!");
+}
+
+int HttpFile::Open(const std::string &url) {
+    url_ = url;
+    double size = 0;
+    for (int count = 0; count < 5 && size <= 0; ++count) {
+        size = http.getHttpFileSize(url);
+        if (size <= 0) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+    }
+    if (size <= 0) {
+        return FILEERR;
+    }
+	std::lock_guard<std::mutex> lock(worker.mtx_);
+    worker.file_size = static_cast<size_t>(size);
+    worker.running = true;
+    worker.download_thread = std::thread(&HttpFile::DownloadThread, this);
+    return FILEOK;
+}
+
+//跳转指定大小 0 success ,-1 error , -2 end OK
+int HttpFile::SeekTo(size_t size) {
+    if (worker.SeekOverflow(size)) {
+        return FILEERR;
+    }
+    // todo: seek需要释放无用的内存
+    std::unique_lock<std::mutex>  cache_lock(cache_.mtx);
+    worker.Seek(size);
+    for (auto iter = cache_.buffers.begin(); iter != cache_.buffers.end();) {
+        if (iter->first < worker.cache_index) {
+            cache_.buffers.erase(iter++);
+        } else {
+            iter++;
+        }
+    }
+    synchronizer.cond.notify_all();
+    return FILEOK;
+}
+
+int HttpFile::Seek(int delta) {
+	if (worker.SeekOverflow(delta)) {
+		return FILEERR;
+	}
+	worker.Seek(delta);
+	synchronizer.cond.notify_all();
+	return FILEOK;
+}
+
+//读取指定大小 返回读取大小 -2 end 读取完成后指针会延后 OK
+int HttpFile::ReadInternal(uint8_t *buf, size_t bufSize, size_t readSize) {
+    hasRead = 0;
+    size_t endRead = readSize;
+    int outTime = 3000 * 2;  // *5 后超时
+    int maxNoticTime = 200;  //多久开始提醒正在读取中
+    if (worker.FileEnd()) {
+        return FILEEND;
+    }
+    bool isWait = false;
+    while (worker.running && hasRead != readSize) {
+        if (worker.FileEnd()) {
+            return FILEEND;
+        }
+        auto key = (worker.current_position ) / kCacheLength;
+	    std::unique_lock<std::mutex>  buf_lock(cache_.mtx);
+        auto v = cache_.buffers.find(key);
+        if (v == cache_.buffers.end()) {
+            //TODO: 激活读取线程 ，等待读取,并且阻塞所有读取线程
+	        buf_lock.unlock();
+            synchronizer.cond.notify_all();
+            //TODO: 阻塞所有读取函数
+            std::this_thread::sleep_for(std::chrono::milliseconds(40));
+            if (maxNoticTime <= 0) {
+                isWait = true;
+                maxNoticTime = 400;
+            }
+            maxNoticTime--;
+            if (outTime-- <= 0) {
+                return FILEERR;
+            }
+            continue;
+        } else {
+            //TODO： 找到并读取
+            //如果足够读取,则读取并退出
+            if ((v->second->end + 1 - worker.current_position ) >= endRead) {
+                //memcpy(buf + hasRead, v->second->data + (current_position - v->second->begin), endRead);
+                v->second->Read(buf + hasRead, worker.current_position , endRead);
+                hasRead += endRead;
+                worker.current_position  += endRead;
+                if (!cache_.enough()) {
+                    synchronizer.cond.notify_all();
+                }
+	            buf_lock.unlock();
+                break;
+            } else {  //不够读取
+                //memcpy(buf + hasRead, v->second->data + (current_position - v->second->begin), v->second->end + 1 - current_position);
+                v->second->Read(buf + hasRead, worker.current_position );
+                endRead -= (v->second->end + 1 - worker.current_position );
+                hasRead += (v->second->end + 1 - worker.current_position );
+                worker.current_position  = v->second->end + 1;
+                cache_.buffers.erase(v);
+            }
+        }
+        if (!cache_.enough()) {
+            synchronizer.cond.notify_all();
+        }
+	    buf_lock.unlock();
+    }
+    return readSize;
+}
+
+int HttpFile::Read(uint8_t *buf, size_t bufSize, size_t readSize) {
+    return Read(buf, bufSize, readSize, 15);
+}
+
+int HttpFile::Read(uint8_t *buf, size_t bufSize, size_t readSize, size_t head_size) {
+    auto ret = ReadInternal(buf, bufSize, readSize);
+    if (ret == FILEEND) {
+        return ret;
+    }
+
+    if (ret != readSize) {
+        auto delta = static_cast<int>(hasRead + head_size);
+        Seek(-delta);
+    }
+    return ret;
+}
+//读取后指针不会延迟 OK
+int HttpFile::ReadDelay(uint8_t *buf, size_t bufSize, size_t readSize) {
+    size_t hasRead = 0;
+    size_t endRead = readSize;
+    size_t seekTmp = worker.current_position;
+    if (worker.FileEnd()) {
+        return FILEEND;
+    }
+    bool isWait = false;
+    int maxNoticTime = 10;   //多久开始提醒正在读取中
+    int outTime = 3000 * 2;  // *5 后超时
+#if defined(SEEK_TEST)
+    while (worker.running && hasRead != readSize && readOption().RunState.isRunning() && !newseek) {
+#else
+    while (worker.running && hasRead != readSize) {
+#endif
+        auto key = (worker.current_position ) / kCacheLength;
+	    std::unique_lock<std::mutex>  buf_lock(cache_.mtx);
+        auto v = cache_.buffers.find(key);
+        if (v == cache_.buffers.end()) {
+            //TODO: 激活读取线程 ，等待读取,并且阻塞所有读取线程
+	        buf_lock.unlock();
+            synchronizer.cond.notify_all();
+            //TODO: 阻塞所有读取函数
+            std::this_thread::sleep_for(std::chrono::milliseconds(40));
+            if (maxNoticTime <= 0) {
+                isWait = true;
+                maxNoticTime = 400;
+            }
+            maxNoticTime--;
+            if (outTime-- <= 0) {
+                return FILEERR;
+            }
+            continue;
+        } else {
+            //TODO： 找到并读取
+            //如果足够读取,则读取并退出
+            if (v->second->end < worker.current_position) {
+	            buf_lock.unlock();
+                return FILEEND;
+            } else if ((v->second->end + 1 - worker.current_position ) >= endRead) {
+                //memcpy(buf + hasRead, v->second->data + (current_position - v->second->begin), endRead);
+                v->second->Read(buf + hasRead, worker.current_position , endRead);
+                hasRead += endRead;
+                worker.current_position  += endRead;
+                if (!cache_.enough()) {
+                    synchronizer.cond.notify_all();
+                }
+	            buf_lock.unlock();
+                break;
+            } else {  //不够读取
+                //memcpy(buf + hasRead, v->second->data + (current_position - v->second->begin), v->second->end + 1 - current_position);
+                v->second->Read(buf + hasRead, worker.current_position );
+                endRead -= (v->second->end + 1 - worker.current_position );
+                hasRead += (v->second->end + 1 - worker.current_position );
+                worker.current_position  = v->second->end + 1;
+            }
+        }
+	    buf_lock.unlock();
+    }
+    if (isWait) {
+    }
+    worker.current_position  = seekTmp;
+    return readSize;
+}
+//回跳到文件开始 OK
+int HttpFile::SeekToBegin() {
+    std::unique_lock<std::mutex> cache_lock(cache_.mtx);
+    worker.cache_index = 0;
+    worker.current_position  = 0;
+    return FILEOK;
+}
+//OK
+void HttpFile::Close() {
+	std::lock_guard<std::mutex> lock(worker.mtx_);
+    if (!worker.running) {
+        return;
+    }
+	{
+		std::lock_guard<std::mutex> lock(close_Mx_);
+        worker.running = false;
+        //worker.curl_agent.Terminate();
+	}
+
+    do {
+        synchronizer.cond.notify_all();
+        if (worker.download_thread.joinable()) {
+            worker.download_thread.join();
+            break;
+        }
+    } while (true);
+    cache_.buffers.clear();
+}
+
+}  // namespace duobei
+
